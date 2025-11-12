25a1342c0fd638b803df86a9e707ecb47d583c8e
Harshit Jain <jainharshit2132005@gmail.com>
2025-11-12 19:25:18 +0530

    Changed default INR to USD

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -65,7 +65,7 @@ function PaymentContent() {
         },
         body: JSON.stringify({
           amount: totalNumber,
-          currency: 'INR',
+          currency: 'USD',
           paymentType: paymentType,
         }),
       })
